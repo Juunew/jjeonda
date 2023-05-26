709b5c9f5a434959e3ce17eba6cd95e68bb92430
Juuunew <hyukjuun@gmail.com>
2023-05-26 14:48:05 +0900

    first commit - project start

---
+++ src/main/java/com/fintech/jjeondaproject/JjeondaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.jjeondaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjeondaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjeondaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fintech/jjeondaproject/JjeondaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.jjeondaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjeondaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
