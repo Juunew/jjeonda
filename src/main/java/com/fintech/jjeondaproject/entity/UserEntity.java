612d79dee7955ffccd7fe099a1299587b3094d97
HiHyerin <songhr95@gmail.com>
2023-05-30 16:56:53 +0900

    UserEntity작성

--- src/main/java/com/fintech/jjeondaproject/entity/UserEntity.java
+++ src/main/java/com/fintech/jjeondaproject/entity/UserEntity.java
@@ -1,4 +1,40 @@
 package com.fintech.jjeondaproject.entity;
 
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // identity, sequence, table, auto
+	private Long userId;
+	@Column(nullable = false, length = 200)
+	private String id;
+	@Column(nullable = false, length = 200)
+	private String password;
+	@Column(nullable = false, length = 200)
+	private String name;
+	@Column(nullable = false, length = 200)
+	private String phone;
+	@Column(nullable = false, length = 200)
+	private String gender;
+	@Column(nullable = false, length = 200)
+	private String birth;
+	@Column(nullable = false, length = 200)
+	private String email;
+	@Column(nullable = false, length = 200)
+	private Date regDate;
+	@Column(nullable = false, length = 200)
+	private Character agreementYn;
 }
