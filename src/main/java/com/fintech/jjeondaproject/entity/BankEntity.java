78e868b3f7c6e6d2a396ae560cf5e3d9f177215d
juunew <hyukjuun@gmail.com>
2023-05-31 10:35:12 +0900

    hyukjun feature #2 - Bank Entity 설계

--- src/main/java/com/fintech/jjeondaproject/entity/BankEntity.java
+++ src/main/java/com/fintech/jjeondaproject/entity/BankEntity.java
@@ -1,4 +1,29 @@
 package com.fintech.jjeondaproject.entity;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "bank")
+@Getter
+@Entity
 public class BankEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bank_id")
+    private Long id;
+
+    @Column(name = "bank_code", length = 3)
+    private String bankCode;
+
+    @Column(name = "card_code", length = 3)
+    private String cardCode;
+
+    @Column(name = "bank_name")
+    private String bankName;
 }
