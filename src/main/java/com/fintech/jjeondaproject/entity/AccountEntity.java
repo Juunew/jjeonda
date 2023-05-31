46fad454ee331c4f08d56a93933c4a70ab5a6996
songhee323 <songhee323@gmail.com>
2023-05-31 10:12:49 +0900

    account entity 설계

--- src/main/java/com/fintech/jjeondaproject/entity/AccountEntity.java
+++ src/main/java/com/fintech/jjeondaproject/entity/AccountEntity.java
@@ -1,4 +1,55 @@
 package com.fintech.jjeondaproject.entity;
 
+import java.sql.Time;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountEntity {
+	
+	@Column(nullable=false)
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long accountId;
+	
+	@Column(nullable=false)
+	private long userId;
+	
+	@Column(nullable=false)
+	private long bankId;
+	
+	@Column(nullable=false)
+	private long accountNum;
+	
+	@Column(nullable=false)
+	private long availableAmt;
+	
+	@Column(nullable=false)
+	Date tranDate;
+	
+	@Column(nullable=false)
+	Time tranTime;
+	
+	@Column(nullable=false, length=2)
+	private String inoutType;
+	
+	@Column(nullable=false)
+	private String content;
+	
+	@Column(nullable=false)
+	private int tranAmt;
+	
+	@Column(nullable=false)
+	private int tranAfterAmt;
+	
 }
