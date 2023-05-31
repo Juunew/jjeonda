65e97daf97fc625344c6db67831a2a9c924b9f4c
songhee323 <songhee323@gmail.com>
2023-05-31 13:55:00 +0900

    수정

--- src/main/java/com/fintech/jjeondaproject/entity/AccountEntity.java
+++ src/main/java/com/fintech/jjeondaproject/entity/AccountEntity.java
@@ -8,13 +8,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
+
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="account")
+@Getter
+@Entity
 public class AccountEntity {
 	
 	@Column(nullable=false)
