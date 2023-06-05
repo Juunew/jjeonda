a356cec7ab8f7b808938422adeaf8699c54a82b1
HyunJoonCho92 <9hjcho2@gmail.com>
2023-06-05 11:20:08 +0900

    card html 구조 조정

--- src/main/java/com/fintech/jjeondaproject/controller/CardController.java
+++ src/main/java/com/fintech/jjeondaproject/controller/CardController.java
@@ -12,37 +12,47 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/cardTest")
+@RequestMapping("/cards")
 public class CardController {
     private  final CardTestService cardTestService;
+
+    @GetMapping("/test")
+    public String testing(){
+        return "test";
+    }
+    @GetMapping
+    public String home(){
+
+        return "card/cardIndex";
+    }
 
     //전체 카드 리스트 조회
     @GetMapping("/list")
     public String cardList(Model model){
         List<CardListDto> cardListDto = cardTestService.selectAll();
         model.addAttribute("cardList", cardListDto);
-
 //        System.out.println(cardListDto);
-        return "cardTest";
+        return "card/cardList";
     }
 
-    // 로그인한 유저의 카드 리스트 조회
+    //    카드사 별 보유 카드 목록
+//    @GetMapping("/list?bankId=")
+//    public String cardListByBankId(@RequestParam("bankId") String bankId, Model model){
+//        List<CardListDto> cardListDto = cardTestService.cardListByBankId();
+//        model.addAttribute("cardList", cardListDto);
+//
+////        System.out.println(cardListDto);
+//        return "cardTest";
+//    }
 
-    // 로그인한 유저의 카드사별 카드 리스트 조회
-    @GetMapping("/list/{bankId}")
-    public String cardListByBankId(@PathVariable("bankId") String cardId, Model model){
-        List<CardListDto> cardListDto = cardTestService.cardListByBankId();
-        model.addAttribute("cardList", cardListDto);
-
-//        System.out.println(cardListDto);
-        return "cardTest";
-    }
-
-    // 카드 상세페이지 조회
-    @GetMapping("/list/{cardId}")
+    //    카드 별 상세 조회
+    @GetMapping("/detail/{cardId}")
     public String cardDetail(@PathVariable("cardId") String cardId, Model model){
         CardDto cardDto = cardTestService.selectOneByCardId(Long.parseLong(cardId));
         model.addAttribute("cardDetail", cardDto);
-        return "cardDetail";
+        return "card/cardDetail";
     }
+
+    //    카드 별명 설정 페이지
+//    @PutMapping("/{cardId}/nickname”)
 }
