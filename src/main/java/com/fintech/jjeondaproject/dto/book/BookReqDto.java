9a0bffef69f12d85bc68769abadf61d8b12a2697
kimekyoung <ek970821111@gmail.com>
2023-06-05 10:21:17 +0900

    가계부 수정 페이지 코드 - 미완성

--- src/main/java/com/fintech/jjeondaproject/dto/book/BookReqDto.java
+++ src/main/java/com/fintech/jjeondaproject/dto/book/BookReqDto.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Getter
 public class BookReqDto {
-
+// 가계부 수정 페이지에서 사용할 것 들만
     private String costType;
     private String content;
     private int cost;
